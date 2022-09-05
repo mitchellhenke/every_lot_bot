9c29d044706a5b17b633315e3e7b277dc18b61b8
Mitchell Henke <mitchell@mitchellhenke.com>
2022-09-05 09:40:50 -0500

    remove old stuff

--- lib/every_lot_bot.ex
+++ lib/every_lot_bot.ex
@@ -4,54 +4,6 @@ defmodule EveryLotBot do
   """
   NimbleCSV.define(MyCSV, newlines: ["\n"])
 
-  @doc """
-  Hello world.
-
-
-
-    el = EveryLot(args.database,
-                  logger=logger,
-                  print_format=args.print_format,
-                  search_format=args.search_format,
-                  id_=args.id)
-
-    if not el.lot:
-        logger.error('No lot found')
-        return
-
-    logger.debug('%s addresss: %s zip: %s', el.lot['id'], el.lot.get('address'), el.lot.get('zip'))
-    logger.debug('db location %s,%s', el.lot['lat'], el.lot['lon'])
-
-    # Get the streetview image and upload it
-    # ("sv.jpg" is a dummy value, since filename is a required parameter).
-    image = el.get_streetview_image(api.config['streetview'])
-    media = api.media_upload('sv.jpg', file=image)
-
-    # compose an update with all the good parameters
-    # including the media string.
-    update = el.compose(media.media_id_string)
-    logger.info(update['status'])
-
-    if not args.dry_run:
-        logger.debug("posting")
-        status = api.update_status(**update)
-        try:
-            el.mark_as_tweeted(status.id)
-        except AttributeError:
-            el.mark_as_tweeted('1')
-  1529 N VAN BUREN ST, 53202
-  CREATE TABLE lots(
-  id TEXT,
-  lat REAL,
-  lon REAL,
-  address TEXT,
-  city TEXT,
-  state TEXT,
-  zip INT,
-  tweeted TEXT
-  );
-  """
-
   def get_streetview_image(property) do
     search_query = "#{property.address}, #{property.city}, WI"
     url = "https://maps.googleapis.com/maps/api/streetview"
