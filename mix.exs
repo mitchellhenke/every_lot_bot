9a426f159628e6b94be65c1990df9606d8b240e8
Mitchell Henke <mitchell@mitchellhenke.com>
2024-11-24 10:34:55 -0600

    update elixir/erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EveryLotBot.MixProject do
     [
       app: :every_lot_bot,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
